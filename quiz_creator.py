67a5a69c93bb2d4ad72fb4770a5eb20cbb036f03
my name <you@example.com>
2025-04-14 17:23:54 +0800

    quiz creator: making a question label and question box to type in problems/questions

--- quiz_creator.py
+++ quiz_creator.py
@@ -6,9 +6,40 @@ from tkinter import messagebox
 root = tk.Tk()
 root.title("Quiz Creator")
 root.geometry("1366x768")
+root.resizable(False, False)
 root.config(background="pink")
 
-# elements
+# Elements
+# Question box
+question_label = tk.Label(
+    root,
+    text="QUESTION",
+    background="pink",
+    foreground="black",
+    font=("Times New Roman", 20, "bold")
+)
+
+question_label.grid(
+    row=0,
+    column=0,
+    padx=10,
+    pady=10,
+    sticky="w"
+)
+
+question_box = tk.Text(
+    root,
+    height=25,
+    width=80,
+    font=("Times New Roman", 15, "bold")
+)
+question_box.grid(
+    row=1,
+    column=0,
+    padx=10,
+    pady=0,
+)
+
 # integrate code from reference
 
 
