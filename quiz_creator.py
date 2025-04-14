408e61234daeac44de78b9a3312e3ebd8cf13712
my name <you@example.com>
2025-04-14 16:03:44 +0800

    quiz creator: initializing tkinter, and setting up of window screen

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,4 +1,16 @@
 # using tkinter create a working gui
- # window
- # elements
+import tkinter as tk
+from tkinter import messagebox
+
+# window
+root = tk.Tk()
+root.title("Quiz Creator")
+root.geometry("1366x768")
+root.config(background="pink")
+
+# elements
 # integrate code from reference
+
+
+# initiate gui
+root.mainloop()
