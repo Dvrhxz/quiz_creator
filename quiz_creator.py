b43102d46c6225e5f375af7b0907858ffb4fd7c1
my name <you@example.com>
2025-04-14 13:39:52 +0800

    quiz creator: program asks for question and choices, choices are stored in a dictionary with its corresponding answer

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,5 +1,15 @@
 # input questions
-# input the possible answers
+while True:
+    question = input("Enter a Question/Problem: ").strip()
+
+    # input the possible answers
+    letter_choices = {}
+    for letter in ["a", "b", "c", "d"]:
+        choice = input(f"enter answer for choice {letter.upper()}: ")
+        letter_choices[letter] = choice
+    break
+
+print(letter_choices)
 # determine the correct answer
 # format and store into a text file
 # make gui (if possible)
