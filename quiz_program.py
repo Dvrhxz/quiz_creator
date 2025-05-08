af72f38b0ca934e617590e880d53387b66001d41
my name <you@example.com>
2025-05-09 05:10:50 +0800

    quiz program: pseudocode

---
+++ quiz_program.py
@@ -0,0 +1,6 @@
+# use tkinter to make gui for quiz
+# randomly take a question from the text file
+# have the user answer the question
+# check if answer is correct
+# if correct pick another question
+# loop until user decides to quit
