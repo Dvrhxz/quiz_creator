455000d5cf9477f983b7d268179a1a859cfa661c
my name <you@example.com>
2025-05-09 05:43:19 +0800

    quiz program: window setup

--- quiz_program.py
+++ quiz_program.py
@@ -1,6 +1,18 @@
 # use tkinter to make gui for quiz
+import tkinter as tk
+
+# quiz window
+root = tk.Tk()
+root.title("Quiz Program")
+root.geometry("1690x900")
+root.resizable(False, False)
+root.config(background="black")
+
+
 # randomly take a question from the text file
 # have the user answer the question
 # check if answer is correct
 # if correct pick another question
 # loop until user decides to quit
+
+root.mainloop()
