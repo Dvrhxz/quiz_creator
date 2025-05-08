15149af7f956f991e400fb8c53ab8f8b02246bb5
my name <you@example.com>
2025-05-09 06:21:58 +0800

    quiz program: main starting ui

--- quiz_program.py
+++ quiz_program.py
@@ -6,7 +6,62 @@ root = tk.Tk()
 root.title("Quiz Program")
 root.geometry("1690x900")
 root.resizable(False, False)
-root.config(background="black")
+root.config(background="pink")
+
+# Elements
+# question box
+question_label = tk.Label(
+    root,
+    text="Question Here",
+    font=("Times New Roman", 20, "bold"),
+    bg="white",
+    fg="black",
+    wraplength=600,
+    justify="center",
+    relief="solid",
+    width=90,
+    height=9
+)
+question_label.pack(pady=20)
+
+# answer buttons and text areas
+answer_frame = tk.Frame(root, bg="pink")
+answer_frame.pack()
+
+answer_buttons = {}
+for index, letter_key in enumerate(["a", "b", "c", "d"]):
+    letter_buttons = tk.Button(
+        answer_frame,
+        text=f"{letter_key.upper()}",
+        font=("Times New Roman", 15, "bold"),
+        bg="green",
+        fg="black",
+        width=5,
+        relief="solid"
+    )
+
+    letter_buttons.grid(
+        row=index,
+        column=0,
+        padx=10,
+        pady=40
+    )
+
+    answer_text = tk.Label(
+        answer_frame,
+        text="Given Here",
+        font=("Times New Roman", 15),
+        bg="pink",
+        fg="black"
+    )
+    answer_text.grid(
+        row=index,
+        column=1,
+        padx=10,
+        pady=5
+    )
+
+    answer_buttons[letter_key] = letter_buttons
 
 
 # randomly take a question from the text file
